ee3b0f99854a5bbd54cf7e7f51cd0354943ebbf3
Fagner Viana de Jesus <fagner.viana35.gmail.com>
2026-03-16 14:42:00 -0300

    atualizando de branch real

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || (import.meta.env.PROD ? '/api' : 'http://localhost:3001')
+  baseURL: (import.meta.env.PROD ? 
+    'https://controle-de-estoque-agape.onrender.com' 
+    : 'http://localhost:3001')
 });
-
 export default api;
